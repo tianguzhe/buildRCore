a92f008947d2fd684d083a09c12dcc2b0fbe501b
yikwing <hubeieasy@gmail.com>
2020-04-15 13:38:46 +0800

    init

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"time"
+)
+
+func main() {
+
+	workdir := "app-arm64-v8a-debug"
+
+	//cmd := exec.Command("rm", "-rf", "./clashR*.apk")
+	//fmt.Printf("%s", "删除旧版本........")
+	//runComd(cmd)
+	//
+	//cmd = exec.Command("java", "-jar", "apktool_2.4.1.jar", "d", "/Users/zhui/AndroidStudioProjects/ClashForAndroid/app/build/outputs/apk/debug/"+workdir+".apk")
+	//fmt.Printf("%s", "正在反编译.........")
+	//runComd(cmd)
+
+	cmd := exec.Command("rm", "-rf", "./"+workdir+"/lib")
+	fmt.Printf("%s", "正在替换内核........")
+	runComd(cmd)
+
+	cmd = exec.Command("cp", "-r", "./lib", "./"+workdir+"/lib")
+	fmt.Printf("%s", "替换内核成功........")
+	runComd(cmd)
+
+	cmd = exec.Command("java", "-jar", "apktool_2.4.1.jar", "b", workdir, "-o", "new.apk")
+	fmt.Printf("%s", "正在重新打包........")
+	runComd(cmd)
+
+	format := time.Now().Format("20060102-150304")
+	cmd = exec.Command("./apksigner", "sign", "--ks", "./clashr.jks", "--ks-key-alias", "key0", "--ks-pass", "pass:123456", "--out", "./clashR"+format+".apk", "./new.apk")
+	fmt.Printf("%s", "重签名成功........")
+	runComd(cmd)
+
+	cmd = exec.Command("rm", "-rf", "./app-debug")
+	runComd(cmd)
+	cmd = exec.Command("rm", "-rf", "./new.apk")
+	fmt.Printf("%s", "清理目录........")
+	runComd(cmd)
+
+}
+
+func runComd(cmd *exec.Cmd) {
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 保证关闭输出流
+	defer stdout.Close()
+	// 运行命令
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	// 读取输出结果
+	opBytes, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(opBytes))
+}
